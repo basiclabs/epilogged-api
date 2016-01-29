fcd5ee5520dfc24e19ee005ca9dcb579eb6307a2
Nicholas Thomson <nthomsondev@gmail.com>
2016-01-28 21:59:35 -0500

    I'm dumb

--- lib/card_catalog/search.rb
+++ lib/card_catalog/search.rb
@@ -4,13 +4,6 @@ class CardCatalog::Search
     def request
       @request ||= Vacuum.new
       @request.associate_tag = ENV['AWS_ASSOCIATE_TAG']
-
-      @request.configure(
-        aws_access_key_id: 'AKIAJCUV6ROVDN3OZREQ',
-        aws_secret_access_key: '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',
-        associate_tag: 'epilogged-20'
-      )
-
       @request
     end
 
